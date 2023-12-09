766614451501dbee7a0faeb3c682f95de60fe0f6
Brynjar <brynjarorng@gmail.com>
2023-12-09 01:23:38 +0000

    cleanup

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, any};
+use std::fs;
 
 struct GameInfo {
     id: i32,
@@ -30,63 +30,7 @@ fn chall1<'a>(input_lines: &'a Vec<&'a str>) -> i32 {
     let green_max = 13;
     let blue_max = 14;
     let mut acc = 0;
-    let mut success = true;
-
-    let mut parsed_games: Vec<GameInfo> = vec![];
-
-    for games in input_lines {
-        success = true;
-        let tmp = games;
-        
-        let split_string = tmp.split(':').collect::<Vec<&str>>();
-    
-        let mut game: GameInfo = GameInfo::default();
-    
-        let game_id = split_string[0].get(5..).unwrap().parse::<i32>().unwrap();
-        game.id = game_id;
-    
-        let balls = split_string[1].replace(";", ",");
-    
-        for ball in balls.split(",") {
-            let ball_parts = ball.split_whitespace().collect::<Vec<&str>>();
-            if ball_parts[1] == "red" {
-                game.red += ball_parts[0].parse::<i32>().unwrap();
-                if ball_parts[0].parse::<i32>().unwrap() > red_max {
-                    success = false
-                }
-            }
-            if ball_parts[1] == "blue" {
-                game.blue += ball_parts[0].parse::<i32>().unwrap();
-                if ball_parts[0].parse::<i32>().unwrap() > blue_max {
-                    success = false
-                }
-            }
-            if ball_parts[1] == "green" {
-                game.green += ball_parts[0].parse::<i32>().unwrap();
-                if ball_parts[0].parse::<i32>().unwrap() > green_max {
-                    success = false
-                }
-            }   
-        }
-
-        if success {
-            acc += game.id;
-        }
-
-        parsed_games.push(game);
-    }
-
-    return acc
-}
-
-fn chall2<'a>(input_lines: &'a Vec<&'a str>) -> i32 {
-    let red_max = 12;
-    let green_max = 13;
-    let blue_max = 14;
-    let mut acc = 0;
-    let mut success = true;
-
-    let mut parsed_games: Vec<GameInfo> = vec![];
+    let mut success;
 
     for games in input_lines {
         success = true;
@@ -105,34 +49,66 @@ fn chall2<'a>(input_lines: &'a Vec<&'a str>) -> i32 {
             let ball_parts = ball.split_whitespace().collect::<Vec<&str>>();
             let val = ball_parts[0].parse::<i32>().unwrap();
             if ball_parts[1] == "red" {
+                if val > red_max {
+                    success = false
+                }
+            }
+            if ball_parts[1] == "blue" {
+                if val > blue_max {
+                    success = false
+                }
+            }
+            if ball_parts[1] == "green" {
+                if val > green_max {
+                    success = false
+                }
+            }   
+        }
+
+        if success {
+            acc += game.id;
+        }
+    }
+
+    return acc
+}
+
+fn chall2<'a>(input_lines: &'a Vec<&'a str>) -> i32 {
+    let mut acc = 0;
+
+    for games in input_lines {
+        let tmp = games;
+        
+        let split_string = tmp.split(':').collect::<Vec<&str>>();
+    
+        let mut game: GameInfo = GameInfo::default();
+    
+        let game_id = split_string[0].get(5..).unwrap().parse::<i32>().unwrap();
+        game.id = game_id;
+    
+        let balls = split_string[1].replace(";", ",");
+    
+        for ball in balls.split(",") {
+            let ball_parts = ball.split_whitespace().collect::<Vec<&str>>();
+            let val = ball_parts[0].parse::<i32>().unwrap();
+            if ball_parts[1] == "red" {
                 if val > game.red {
                     game.red = val;
-                }
-                if ball_parts[0].parse::<i32>().unwrap() > red_max {
-                    success = false
                 }
             }
             if ball_parts[1] == "blue" {
                 if val > game.blue {
                     game.blue = val;
                 }
-                if ball_parts[0].parse::<i32>().unwrap() > blue_max {
-                    success = false
-                }
             }
             if ball_parts[1] == "green" {
                 if val > game.green {
                     game.green = val;
                 }
-                if ball_parts[0].parse::<i32>().unwrap() > green_max {
-                    success = false
-                }
             }   
         }
 
         acc += game.red * game.blue * game.green;
-
-        parsed_games.push(game);
     }
 
     return acc
@@ -142,8 +118,9 @@ fn main() {
     let file_contents: String = fs::read_to_string("./src/input").expect("File should have been read");
     let split_lines = words_by_line(&file_contents);
 
-    // let parsed_data = chall1(&split_lines);
-    let parsed_data = chall2(&split_lines);
+    let chall1 = chall1(&split_lines);
+    let chall2 = chall2(&split_lines);
 
-    println!("{:?}", parsed_data);
+    println!("{:?}", chall1);
+    println!("{:?}", chall2);
 }
